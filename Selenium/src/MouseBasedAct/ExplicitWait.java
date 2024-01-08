d1e644b6d9798da568f0e5d2f95d0efa444e926b
AKASH PANDHARE <akashpandhare30@gmail.com>
2024-01-08 09:04:34 +0530

    ExplicitWaitCondition

---
+++ Selenium/src/MouseBasedAct/ExplicitWait.java
@@ -0,0 +1,24 @@
+package MouseBasedAct;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitWait {
+	public static void main(String[] args) {
+		ChromeDriver driver=new  ChromeDriver();
+		driver.manage().window().maximize();
+		
+		WebDriverWait  Wait=new WebDriverWait(driver,Duration.ofSeconds(15));
+		
+		driver.get("https://demowebshop.tricentis.com/");
+		driver.findElement(By.xpath("//input[@id='small-searchterms']")).sendKeys("computers");
+		
+		Wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@value='Search']")));
+		
+		driver.findElement(By.xpath("//input[@value='Search']")).click();
+}
+}
